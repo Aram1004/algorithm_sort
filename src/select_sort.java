cefd179ec6aa07b9ad2f881261a8d19275350eb5
Aram1004 <ahram970423@gmail.com>
2019-10-11 17:41:46 +0900

    algorithm_Sort

---
+++ src/select_sort.java
@@ -0,0 +1,34 @@
+public class select_sort {
+    private static void selectionSort(int[] arr) {
+        selectionSort(arr, 0);
+    }
+    private  static void selectionSort(int[] arr, int start) {
+        if (start< arr.length -1) {
+            int min_index = start;
+            for (int i = start; i < arr.length; i++){
+                if (arr[i] < arr[min_index]) min_index =i;
+            }
+            swap(arr, start, min_index);
+            selectionSort(arr, start+1);
+        }
+    }
+    public static void swap( int[] arr, int index1, int index2){
+        int tmp= arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+    private static void printArray(int[] arr){
+        for (int data : arr){
+            System.out.print(data+",");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int[] arr = {6,1,8,3,2,4};
+        printArray(arr);
+        selectionSort(arr);
+        printArray(arr);
+
+    }
+}
